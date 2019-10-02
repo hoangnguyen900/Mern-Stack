17439e28c392d639ad314e60087b275753f2bb11
hoangnguyen900 <hoang.nguyen@terralogic.com>
2019-10-02 14:01:44 +0700

    ??

--- FrontEnd/src/components/QuizCreatorQuestionInput/QuizCreatorQuestionInput.js
+++ FrontEnd/src/components/QuizCreatorQuestionInput/QuizCreatorQuestionInput.js
@@ -9,28 +9,6 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 
 class QuizCreatorQuestionInput extends React.Component {
-<<<<<<< HEAD
-    constructor(props) {
-        super(props);
-        this.state = {};
-    }
-    render() {
-        var { index } = this.props;
-        return (
-            <div className="question-input">
-                <FontAwesomeIcon icon={faCheckCircle} size="2x" color="#CAD2DC" />
-                <div className="input-group">
-                    <input type="text" />
-                    <span><FontAwesomeIcon icon={faTrashAlt} /></span>
-
-                </div>
-                <span><FontAwesomeIcon icon={faSquareRootAlt} size="2x" color="#CAD2DC" /></span>
-                <span><FontAwesomeIcon icon={faImage} size="2x" color="#CAD2DC" /></span>
-
-            </div>
-        );
-    }
-=======
   constructor(props) {
     super(props);
     this.state = {
@@ -66,7 +44,6 @@ class QuizCreatorQuestionInput extends React.Component {
       </div>
     );
   }
->>>>>>> e627c359dce27f17df68a2803ff232d515d4e77f
 }
 
 export default QuizCreatorQuestionInput;
