e627c359dce27f17df68a2803ff232d515d4e77f
hoangnguyen900 <hoang.nguyen@terralogic.com>
2019-10-02 13:53:20 +0700

    ???

--- FrontEnd/src/components/QuizCreatorQuestionInput/QuizCreatorQuestionInput.js
+++ FrontEnd/src/components/QuizCreatorQuestionInput/QuizCreatorQuestionInput.js
@@ -2,30 +2,48 @@ import React from "react";
 import "./QuizCreatorQuestionInput.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-    faCheckCircle,
-    faSquareRootAlt,
-    faImage,
-    faTrashAlt
+  faCheckCircle,
+  faSquareRootAlt,
+  faImage,
+  faTrashAlt
 } from "@fortawesome/free-solid-svg-icons";
+
 class QuizCreatorQuestionInput extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {};
-    }
-    render() {
-        var { index } = this.props;
-        return (
-            <div className="question-input">
-                <FontAwesomeIcon icon={faCheckCircle} size="2x" color="#CAD2DC" />
-                <div className="input-group">
-                    <input type="text" />
-                    <span><FontAwesomeIcon icon={faTrashAlt} /></span>
-                </div>
-                <FontAwesomeIcon icon={faSquareRootAlt} size="2x" color="#CAD2DC" />
-                <FontAwesomeIcon icon={faImage} size="2x" color="#CAD2DC" />
-            </div>
-        );
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      isDisplayDelIcon: true
+    };
+  }
+  handleOnclickDelete = () => {};
+  componentDidMount = () => {
+    let display = false;
+    this.props.index > 2 ? (display = true) : (display = false);
+    this.setState({
+      isDisplayDelIcon: display
+    });
+    console.log(this.state);
+  };
+  render() {
+    var { isDisplayDelIcon } = this.state;
+
+    return (
+      <div className="question-input">
+        <FontAwesomeIcon icon={faCheckCircle} size="2x" color="#CAD2DC" />
+        <div className="input-group">
+          <input type="text" />
+          <span
+            onClick={this.handleOnclickDelete}
+            style={{ display: isDisplayDelIcon ? "block" : "none" }}
+          >
+            <FontAwesomeIcon icon={faTrashAlt} />
+          </span>
+        </div>
+        <FontAwesomeIcon icon={faSquareRootAlt} size="2x" color="#CAD2DC" />
+        <FontAwesomeIcon icon={faImage} size="2x" color="#CAD2DC" />
+      </div>
+    );
+  }
 }
 
 export default QuizCreatorQuestionInput;
