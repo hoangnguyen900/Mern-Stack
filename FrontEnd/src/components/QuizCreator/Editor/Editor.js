566961fd2f349fbb639d66c923b97ce22069a624
Minh Tri <handcock.dk@gmail.com>
2019-10-08 19:38:55 +0700

    ??

--- FrontEnd/src/components/QuizCreator/Editor/Editor.js
+++ FrontEnd/src/components/QuizCreator/Editor/Editor.js
@@ -1,17 +1,13 @@
 import React from "react";
-import "./Editor.scss";
-<<<<<<< HEAD
-=======
-
+import './Editor.scss';
 //import ToggleBox from '../ToggleBox/ToggleBox';
 import QuizCreatorQuestionDetail from "../QuestionDetail/QuestionDetail";
 
->>>>>>> 180a036e6313c21a74787209ce3b15cb68fc542d
 import "font-awesome/css/font-awesome.min.css";
 //import ToggleBox from '../ToggleBox/ToggleBox';
 import { connect } from "react-redux";
 import * as actions from "./../../../redux/actions/index";
-import QuizCreatorQuestionDetail from '../QuestionDetail/QuestionDetail';
+
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlusCircle, faSave } from "@fortawesome/free-solid-svg-icons";
