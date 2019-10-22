c7d4f932c2202694df6019921ef8db8654b08db9
Minh Tri <handcock.dk@gmail.com>
2019-10-22 15:56:53 +0700

    ??

--- FrontEnd/src/utils/QuizThumbnail/QuizDetailTable/QuizDetailTable.js
+++ FrontEnd/src/utils/QuizThumbnail/QuizDetailTable/QuizDetailTable.js
@@ -4,8 +4,8 @@ import './QuizDetailTable.scss';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
     faTimesCircle,
-   
-  } from "@fortawesome/free-solid-svg-icons";
+
+} from "@fortawesome/free-solid-svg-icons";
 class QuizDetailTable extends React.Component {
     constructor(props) {
         super(props);
@@ -15,62 +15,64 @@ class QuizDetailTable extends React.Component {
     }
     render() {
         return (
-            <div className="popup">
-                <div className="popup_inner">
-                    <div className="popup-header">
-                        
-                        <img src={require("../images/thumbnail.jpg")} alt="thumbnail"/>
-                        <button><FontAwesomeIcon icon={faTimesCircle} size="2x" color={"#60615F"}/></button>
-                        <div className="quiz-flat-info">
-                            <div className="question-number">
-                                15 Qs
+            <div>
+                <div className="popup-quiz-detail-table">
+                    <div className="popup_inner-quiz-detail-table">
+                        <div className="popup-header-quiz-detail-table">
+
+                            <img src={require("../images/thumbnail.jpg")} alt="thumbnail" />
+                            <button><FontAwesomeIcon icon={faTimesCircle} size="2x" color={"#60615F"} /></button>
+                            <div className="quiz-flat-info">
+                                <div className="question-number">
+                                    15 Qs
                             </div>
-                            <div className="play-number">
-                                10 plays
+                                <div className="play-number">
+                                    10 plays
                             </div>
-                        </div>
-                        <div className="quiz-name">
-                            <span>
-                                Basic English
+                            </div>
+                            <div className="quiz-name">
+                                <span>
+                                    Basic English
                             </span>
-                        </div>
-                        <div className="author-container">
-                            <img src={require("../images/ava.png")} alt="ava"/>
-                            <div className="author-name">
-                                Tri Hoang
                             </div>
-                            <div className="grade">
-                                Grades: <span> 1th to 2th</span>{/*bind at 1st and 2nd */}
+                            <div className="author-container">
+                                <img src={require("../images/ava.png")} alt="ava" />
+                                <div className="author-name">
+                                    Tri Hoang
                             </div>
-                        </div>
-                        <hr/>
-                    </div>
-                    <div className="popup-body">
-                        <div className="difficult-level">
-                            Difficult level: <span>Medium</span>
-                        </div>
-                        <h5>Sample questions</h5>
-                        <div className="sample-questions-container">
-                            <div className="sample-question">
-                                <div className="sample-content">
-                                    1. What are you doing?????????????????????????????????????????????????????
-                                </div>
-                                <div className="sample-image">
-                                    <img src={require("../images/sample.jpg")} alt="sampleImage"/>
+                                <div className="grade">
+                                    Grades: <span> 1th to 2th</span>{/*bind at 1st and 2nd */}
                                 </div>
                             </div>
-                            <div className="sample-question">
-                                <div className="sample-content">
-                                    1. What are you doing?
+                            <hr />
+                        </div>
+                        <div className="popup-body-quiz-detail-table">
+                            <div className="difficult-level">
+                                Difficult level: <span>Medium</span>
+                            </div>
+                            <h5>Sample questions</h5>
+                            <div className="sample-questions-container">
+                                <div className="sample-question">
+                                    <div className="sample-content">
+                                        1. What are you doing?????????????????????????????????????????????????????
                                 </div>
-                                <div className="sample-image">
-                                    <img src={require("../images/sample.jpg")} alt="sampleImage"/>
+                                    <div className="sample-image">
+                                        <img src={require("../images/sample.jpg")} alt="sampleImage" />
+                                    </div>
+                                </div>
+                                <div className="sample-question">
+                                    <div className="sample-content">
+                                        1. What are you doing?
+                                </div>
+                                    <div className="sample-image">
+                                        <img src={require("../images/sample.jpg")} alt="sampleImage" />
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                    <div className="popup-footer">
-                        <button>Play</button>
+                        <div className="popup-footer-quiz-detail-table">
+                            <button>Play</button>
+                        </div>
                     </div>
                 </div>
             </div>
--- FrontEnd/src/utils/QuizThumbnail/QuizThumbnail.js
+++ FrontEnd/src/utils/QuizThumbnail/QuizThumbnail.js
@@ -4,87 +4,90 @@ import { connect } from "react-redux";
 import * as actions from "./../../redux/actions/index";
 import QuizDetailTable from "./QuizDetailTable/QuizDetailTable";
 class QuizThumbnail extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      data: {
-        id: 0,
-        code: 0,
-        title: "",
-        image: "",
-        played: 0,
-        questions: []
-      },
-      isShowPopup: false
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: {
+                id: 0,
+                code: 0,
+                title: "",
+                image: "",
+                played: 0,
+                questions: []
+            },
+            isShowPopup: false
+        };
+    }
+    componentDidMount() {
+        let { data } = this.props;
+        this.setState({
+            data: data
+        });
+    }
+
+    togglePopup = () => {
+        this.setState({
+            isShowPopup: !this.state.isShowPopup
+        });
     };
-  }
-  componentDidMount() {
-    let { data } = this.props;
-    this.setState({
-      data: data
-    });
-  }
+    render() {
+        let { data } = this.state;
 
-  togglePopup = () => {
-    this.setState({
-      isShowPopup: !this.state.isShowPopup
-    });
-  };
-  render() {
-    let { data } = this.state;
-
-    //console.log("props", this.props.data);
-    return (
-      <div className="quiz-thumbnail-container" onClick={this.togglePopup}>
-        <img src={require("./images/thumbnail.jpg")} alt="thumbnail" />
-        <div className="quiz-flat-info">
-          <div className="question-number">{data.questions.length} Qs</div>
-          <div className="play-number">
-            {data.played !== 0 ? data.played : "0"} plays
+        //console.log("props", this.props.data);
+        return (
+            <div>
+                <div className="quiz-thumbnail-container" onClick={this.togglePopup}>
+                    <img src={require("./images/thumbnail.jpg")} alt="thumbnail" />
+                    <div className="quiz-flat-info">
+                        <div className="question-number">{data.questions.length} Qs</div>
+                        <div className="play-number">
+                            {data.played !== 0 ? data.played : "0"} plays
           </div>
-        </div>
-        <div className="quiz-name">
-          <span>{data.title}</span>
-        </div>
-        <div className="author-name">
-          <span>
-            <span>By:</span> {this.props.userName}
-          </span>
-        </div>
+                    </div>
+                    <div className="quiz-name">
+                        <span>{data.title}</span>
+                    </div>
+                    <div className="author-name">
+                        <span>
+                            <span>By:</span> {this.props.userName}
+                        </span>
+                    </div>
 
-        <div className="progression">
-          <div className="pr-ing">
-            <div className="pr-bar">3 questions left</div>
-          </div>
-        </div>
+                    <div className="progression">
+                        <div className="pr-ing">
+                            <div className="pr-bar">3 questions left</div>
+                        </div>
+                    </div>
 
-        {this.state.isShowPopup ? (
-          <QuizDetailTable togglePopup={this.togglePopup} />
-        ) : null}
+                   
+                    <div className="accuracy">
+                        <div className="pr-ing">
+                            <div className="pr-bar">45% accuracy</div>
+                        </div>
+                    </div>
+                </div>
+                {this.state.isShowPopup ? (
+                        <QuizDetailTable togglePopup={this.togglePopup} />
+                    ) : null}
 
-        <div className="accuracy">
-          <div className="pr-ing">
-            <div className="pr-bar">45% accuracy</div>
-          </div>
-        </div>
-      </div>
-    );
-  }
+            </div>
+        );
+    }
 }
 const mapDispatchToProps = (dispatch, props) => {
-  return {
-    showListQuestionAnswer: question_table_id => {
-      dispatch(actions.showListQuestionAnswer(question_table_id));
-    }
-  };
+    return {
+        showListQuestionAnswer: question_table_id => {
+            dispatch(actions.showListQuestionAnswer(question_table_id));
+        }
+    };
 };
 const mapStateToProps = state => {
-  return {
-    questionTable: state.questionTable,
-    user: state.user
-  };
+    return {
+        questionTable: state.questionTable,
+        user: state.user
+    };
 };
 export default connect(
-  mapStateToProps,
-  mapDispatchToProps
+    mapStateToProps,
+    mapDispatchToProps
 )(QuizThumbnail);
