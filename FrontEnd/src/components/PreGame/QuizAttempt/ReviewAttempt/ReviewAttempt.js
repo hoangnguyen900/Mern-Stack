805b952bd599bfc517f9a196ee56668d21fc60d2
Minh Tri <handcock.dk@gmail.com>
2019-10-26 11:08:32 +0700

    add review component

---
+++ FrontEnd/src/components/PreGame/QuizAttempt/ReviewAttempt/ReviewAttempt.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '.ReviewAttempt.scss';
+class ReviewAttempt extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( 
+            <>
+            </>
+         );
+    }
+}
+ 
+export default ReviewAttempt;
