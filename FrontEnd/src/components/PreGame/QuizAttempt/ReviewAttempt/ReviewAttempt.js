262da1e18db69548f39df954320442c3d888f571
Minh Tri <handcock.dk@gmail.com>
2019-10-26 11:25:06 +0700

    ??

--- FrontEnd/src/components/PreGame/QuizAttempt/ReviewAttempt/ReviewAttempt.js
+++ FrontEnd/src/components/PreGame/QuizAttempt/ReviewAttempt/ReviewAttempt.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import '.ReviewAttempt.scss';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTimes } from "@fortawesome/free-solid-svg-icons";
 class ReviewAttempt extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {  }
+        this.state = {}
     }
-    render() { 
-        return ( 
-            <>
-            </>
-         );
+    render() {
+        return (
+            <div className="review-attempt-container">
+                <div className="review-attempt-nav">
+                    <button>
+                        <span>
+                            <FontAwesomeIcon icon={faTimes} />
+                        </span>
+                    </button>
+                </div>
+            </div>
+        );
     }
 }
- 
+
 export default ReviewAttempt;
