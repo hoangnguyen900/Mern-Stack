59fd9d6a4b30a9c35d619b7d6a74bc18f2080775
hoangnguyen900 <hoang.nguyen@terralogic.com>
2019-11-10 15:15:37 +0700

    router err

--- FrontEnd/src/components/Admin/Page/MyQuizzes/MyQuizDetail/MyQuizControl/MyQuizControl.js
+++ FrontEnd/src/components/Admin/Page/MyQuizzes/MyQuizDetail/MyQuizControl/MyQuizControl.js
@@ -1,123 +1,144 @@
-import React from 'react';
-import './MyQuizControl.scss';
+import React from "react";
+import "./MyQuizControl.scss";
+import { withRouter } from "react-router-dom";
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-    faTrashAlt,
-    faPrint,
-    faBook,
-    faPlay,
-    faAtlas,
-    faUsers,
-    faQuestion,
-    faUser,
-    faComments,
-    faEye,
-    faEyeSlash
+  faTrashAlt,
+  faPrint,
+  faBook,
+  faPlay,
+  faAtlas,
+  faUsers,
+  faQuestion,
+  faUser,
+  faComments,
+  faEye,
+  faEyeSlash
 } from "@fortawesome/free-solid-svg-icons";
 
-import QuizControlQuestionDetail from './QuestionDetail/QuestionDetail'
+import QuizControlQuestionDetail from "./QuestionDetail/QuestionDetail";
 class MyQuizControl extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {}
-    }
-    render() {
-        return (
-            <div className="my-quiz-control-container">
-                <div className="quiz-control-header">
-                    <div className="header-image">
-                        <img alt="header" className="header-img" src={require("../../../../../../utils/QuizThumbnail/images/thumbnail.jpg")} />
-                    </div>
-                    <div className="header-info">
-                        <div className="title-and-func">
-                            <div className="title">basic english</div>
-                            <div className="func-group">
-                                <div className="func">
-                                    <span><FontAwesomeIcon icon={faTrashAlt} /></span>
-                                </div>
-                                <div className="func">
-                                    <span><FontAwesomeIcon icon={faPrint} /></span>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="grade-and-plays">
-                            <div className="grade">
-                                <span><FontAwesomeIcon icon={faBook} color="#6B7C93" /></span>
-                                1 st grade - 2nd grade
-                            </div>
-                            <div className="plays">
-                                <span><FontAwesomeIcon icon={faPlay} color="#6B7C93" /></span>
-                                Played 4 times
-                            </div>
-                        </div>
-                        <div className="subject">
-                            <span><FontAwesomeIcon icon={faAtlas} color="#6B7C93" /></span>
-                            Physic
-                        </div>
-                    </div>
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div className="my-quiz-control-container">
+        <div className="quiz-control-header">
+          <div className="header-image">
+            <img
+              alt="header"
+              className="header-img"
+              src={require("../../../../../../utils/QuizThumbnail/images/thumbnail.jpg")}
+            />
+          </div>
+          <div className="header-info">
+            <div className="title-and-func">
+              <div className="title">basic english</div>
+              <div className="func-group">
+                <div className="func">
+                  <span>
+                    <FontAwesomeIcon icon={faTrashAlt} />
+                  </span>
                 </div>
-                <div className="quiz-control-action-hosting">
-                    <div className="action-group">
-                        <div className="action-group-info">
-                            <div className="action-name">
-                                <span><FontAwesomeIcon icon={faUsers} color="#868790" /></span>
-                                Host a game
-                            </div>
-                            <div className="explain-action">
-                                <span>
-                                    <FontAwesomeIcon icon={faQuestion} size="xs" color="white" />
-                                </span>
-                            </div>
-                        </div>
-                        <div className="action-btn-group">
-                            <button className="action-btn b-host">Live game</button>
-                            <button className="action-btn b-host">Host game</button>
-                        </div>
-                    </div>
-
-                    <div className="action-group">
-                        <div className="action-group-info">
-                            <div className="action-name">
-                                <span><FontAwesomeIcon icon={faUser} color="#868790" /></span>
-                                Solo Practice
-                            </div>
-                            <div className="explain-action">
-                                <span>
-                                    <FontAwesomeIcon icon={faQuestion} size="xs" color="white" />
-                                </span>
-                            </div>
-                        </div>
-                        <div className="action-btn-group">
-                            <button className="action-btn b-pratice">Practice</button>
-                        </div>
-                    </div>
+                <div className="func">
+                  <span>
+                    <FontAwesomeIcon icon={faPrint} />
+                  </span>
                 </div>
-                <div className="quiz-control-action-drafting">
-
-                </div>
-                <hr />
-                <div className="quiz-control-questions">
-                    <div className="questions-group-info">
-                        <div className="question-number">
-                            <span><FontAwesomeIcon icon={faComments} /></span>4 Questions
-                        </div>
-                        <button className="show-answers">
-                            <span>
-                                <FontAwesomeIcon icon={faEye} color="#8A81EC" />
-                            </span>
-                            <span style={{ display: 'none' }}>
-                                <FontAwesomeIcon icon={faEyeSlash} color="#8A81EC" />
-                            </span>
-                            SHOW ANSWERS
-                        </button>
-                    </div>
-                    <div className="question-lists">
-                        <QuizControlQuestionDetail/>
-                    </div>
-                </div>
+              </div>
             </div>
-        );
-    }
+            <div className="grade-and-plays">
+              <div className="grade">
+                <span>
+                  <FontAwesomeIcon icon={faBook} color="#6B7C93" />
+                </span>
+                1 st grade - 2nd grade
+              </div>
+              <div className="plays">
+                <span>
+                  <FontAwesomeIcon icon={faPlay} color="#6B7C93" />
+                </span>
+                Played 4 times
+              </div>
+            </div>
+            <div className="subject">
+              <span>
+                <FontAwesomeIcon icon={faAtlas} color="#6B7C93" />
+              </span>
+              Physic
+            </div>
+          </div>
+        </div>
+        <div className="quiz-control-action-hosting">
+          <div className="action-group">
+            <div className="action-group-info">
+              <div className="action-name">
+                <span>
+                  <FontAwesomeIcon icon={faUsers} color="#868790" />
+                </span>
+                Host a game
+              </div>
+              <div className="explain-action">
+                <span>
+                  <FontAwesomeIcon icon={faQuestion} size="xs" color="white" />
+                </span>
+              </div>
+            </div>
+            <div className="action-btn-group">
+              <button className="action-btn b-host">Live game</button>
+              <button className="action-btn b-host">Host game</button>
+            </div>
+          </div>
+
+          <div className="action-group">
+            <div className="action-group-info">
+              <div className="action-name">
+                <span>
+                  <FontAwesomeIcon icon={faUser} color="#868790" />
+                </span>
+                Solo Practice
+              </div>
+              <div className="explain-action">
+                <span>
+                  <FontAwesomeIcon icon={faQuestion} size="xs" color="white" />
+                </span>
+              </div>
+            </div>
+            <div className="action-btn-group">
+              <button className="action-btn b-pratice">Practice</button>
+            </div>
+          </div>
+        </div>
+        <div className="quiz-control-action-drafting"></div>
+        <hr />
+        <div className="quiz-control-questions">
+          <div className="questions-group-info">
+            <div className="question-number">
+              <span>
+                <FontAwesomeIcon icon={faComments} />
+              </span>
+              4 Questions
+            </div>
+            <button className="show-answers">
+              <span>
+                <FontAwesomeIcon icon={faEye} color="#8A81EC" />
+              </span>
+              <span style={{ display: "none" }}>
+                <FontAwesomeIcon icon={faEyeSlash} color="#8A81EC" />
+              </span>
+              SHOW ANSWERS
+            </button>
+          </div>
+          <div className="question-lists">
+            <QuizControlQuestionDetail />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default MyQuizControl;
+export default withRouter(MyQuizControl);
--- FrontEnd/src/components/Admin/Page/MyQuizzes/MyQuizDetail/MyQuizDetail.js
+++ FrontEnd/src/components/Admin/Page/MyQuizzes/MyQuizDetail/MyQuizDetail.js
@@ -1,44 +1,58 @@
-import React from 'react';
-import './MyQuizDetail.scss';
+import React from "react";
+import "./MyQuizDetail.scss";
+import { withRouter } from "react-router-dom";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {faPlay, faBook} from "@fortawesome/free-solid-svg-icons";
-import history from "../../../../../history"
+import { faPlay, faBook } from "@fortawesome/free-solid-svg-icons";
+import history from "../../../../../history";
 class MyQuizDetail extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {}
-    }
-    render() {
-        console.log(this.props)
-        return (
-            <div className="my-quiz-detail-container" onClick={()=>{
-                history.push("/admin/private/1")
-            }}>
-                <div className="my-quiz-image">
-                    <img alt="myquizimg" src={require("../../../../../utils/QuizThumbnail/images/thumbnail.jpg")}/>
-                </div>
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    console.log(this.props);
+    let { history } = this.props;
+    return (
+      <div
+        className="my-quiz-detail-container"
+        onClick={() => {
+          history.push("/admin/private/1");
+        }}
+      >
+        <div className="my-quiz-image">
+          <img
+            alt="myquizimg"
+            src={require("../../../../../utils/QuizThumbnail/images/thumbnail.jpg")}
+          />
+        </div>
 
-                <div className="my-quiz-info">
-                    <div className="name-and-status">
-                        <div className="name">basic english<span>(4 Qs)</span></div>
-                        <div className="is-drafting">Draft</div>
-                    </div>
-                    <div className="play-detail">
-                        <div className="play">
-                            <span><FontAwesomeIcon icon={faPlay} color="#6B7C93"/></span>
-                            Played 4 times
-                        </div>
-                        <div className="grade">
-                            <span><FontAwesomeIcon icon={faBook} color="#6B7C93"/></span>
-                            1 st grade - 2nd grade
-                        </div>
-                    </div>
-                    <div className="quiz-action"></div>
-                </div>
+        <div className="my-quiz-info">
+          <div className="name-and-status">
+            <div className="name">
+              basic english<span>(4 Qs)</span>
             </div>
-        );
-    }
+            <div className="is-drafting">Draft</div>
+          </div>
+          <div className="play-detail">
+            <div className="play">
+              <span>
+                <FontAwesomeIcon icon={faPlay} color="#6B7C93" />
+              </span>
+              Played 4 times
+            </div>
+            <div className="grade">
+              <span>
+                <FontAwesomeIcon icon={faBook} color="#6B7C93" />
+              </span>
+              1 st grade - 2nd grade
+            </div>
+          </div>
+          <div className="quiz-action"></div>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default MyQuizDetail;
+export default withRouter(MyQuizDetail);
